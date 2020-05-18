d60701901f03e3735add5ef5e5cee30c398759df
Erufaku <65045289+Erufaku@users.noreply.github.com>
2020-05-18 21:23:27 +0300

    Update cstring0.hpp

--- cstring0.hpp
+++ cstring0.hpp
@@ -1,7 +1,7 @@
 /*
  * cstring0.h
  *
- *  Created on: 9 мая 2020 г.
+ *  Created on: 9 Г¬Г Гї 2020 ГЈ.
  *      Author: coolg
  */
 
@@ -21,7 +21,7 @@ public:
     cstring0(const cstring& other):cstring(other){};
 	cstring0(const cstring0 &other);
 	using cstring::operator =;
-	void output(std::ostream &stream) const override final;
+	void output() const override final;
 };
 cstring0 operator+(const cstring& left, const cstring& right);
 #endif /* SRC_CSTRING0_H_ */
